f8c68669b53b9f66f1a9d4ee738919352657de95
Sam Ghitelman <sghitelman@gmail.com>
2025-02-07 15:54:41 -0500

    Add tailwind

--- src/BandForm.js
+++ src/BandForm.js
@@ -1,7 +1,7 @@
 function BandForm({ band }) {
   return (
     <div>
-      <h1>{band.name}</h1>
+      <h1 class="text-5xl font-semibold tracking-tight text-blue-600 sm:text-7xl">{band.name}</h1>
       {band.ticketTypes.map((ticket) => (
         <p>
           {ticket.name} - {ticket.description}
