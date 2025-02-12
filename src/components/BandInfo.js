3d732592a7a434ef967a38a7220e449ea3859fa5
Sam Ghitelman <sghitelman@gmail.com>
2025-02-12 09:59:34 -0500

    Fix bad html in band blurbs

--- src/components/BandInfo.js
+++ src/components/BandInfo.js
@@ -1,10 +1,11 @@
+import DOMPurify from 'dompurify';
 import parse from 'html-react-parser';
 
 export default function BandInfo({ show }) {
   return (
     <div className="bg-gray-100 p-4">
       <img src={show.imgUrl} alt={show.name} className="block w-full"/>
-      {parse(show.description_blurb)}
+      {parse(DOMPurify.sanitize(show.description_blurb))}
     </div>
   )
 }
