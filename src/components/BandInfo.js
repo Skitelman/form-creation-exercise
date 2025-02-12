36a0a6c2fd035d2f0a44157dfb5498bdc7bbfa3b
Sam Ghitelman <sghitelman@gmail.com>
2025-02-12 11:54:33 -0500

    Use placeholder image when image invalid

--- src/components/BandInfo.js
+++ src/components/BandInfo.js
@@ -1,11 +1,20 @@
-import DOMPurify from 'dompurify';
-import parse from 'html-react-parser';
+import { useState } from "react";
+import DOMPurify from "dompurify";
+import parse from "html-react-parser";
+
+const PLACEHOLDER_IMAGE_URL = '/placeholder.jpg';
 
 export default function BandInfo({ show }) {
+  const [imgUrl, setImgUrl] = useState(show.imgUrl);
+
+  function onImgError(e) {
+    e.preventDefault();
+    setImgUrl(PLACEHOLDER_IMAGE_URL);
+  }
   return (
     <div className="bg-gray-100 p-4">
-      <img src={show.imgUrl} alt={show.name} className="block w-full"/>
+      <img src={imgUrl} alt={show.name} className="block w-full" onError={onImgError} />
       {parse(DOMPurify.sanitize(show.description_blurb))}
     </div>
-  )
+  );
 }
