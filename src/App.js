2156b328f35f0b68daf4b9250763931e11f6ae83
Sam Ghitelman <sghitelman@gmail.com>
2025-02-14 11:08:15 -0500

    Basic routing

--- src/App.js
+++ src/App.js
@@ -6,9 +6,31 @@ import BandForm from "./BandForm";
 
 function App() {
   const bands = [skaBand, kpopBand, punkBand];
+
+  function getBand() {
+    const path = window.location.href;
+    const route = path.split("/").pop();
+    switch (route) {
+      case "skaBand":
+      case "0":
+      case "flaming-potatoes":
+        return skaBand;
+      case "kpopBand":
+      case "1":
+      case "btess":
+        return kpopBand;
+      case "punkBand":
+      case "2":
+      case "cindy":
+        return punkBand;
+      default:
+        return skaBand;
+    }
+  }
+
   return (
     <div className="App p-12 font-barlow bg-linear-to-b from-slate-800 from-40% to-gray-700">
-      <BandForm show={bands[1]} />
+      <BandForm show={getBand()} />
     </div>
   );
 }
